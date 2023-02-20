1aab4b4f38bbd20ee395dd31059c5cb7a7cde380
CraigCurrie <CraigCurrie118@gmail.com>
2023-02-20 12:47:06 +0000

    Added some of Players and Added to Player

--- Player.java
+++ Player.java
@@ -4,25 +4,32 @@ public class Player{
     int totalGuesses;
     int cryptogramsPlayed;
     int cryptogramsCompleted;
-    
-public void updateAccuracy(double acc){
-    double combAcc = accuracy + acc;
-    accuracy = combAcc/2;
-}
-public void incrementCryptogramsCompleted(){
-    cryptogramsCompleted = cryptogramsCompleted + 1;
-}
-public void incrementCryptogramsPlayed(){
-    cryptogramsPlayed = cryptogramsPlayed + 1;
-}
-public Double getAccuracy() {
-    return accuracy;
-}
-public int getNumCryptogramsCompleted() {
-    return cryptogramsCompleted;
-}
-public int getNumCryptogramsPlayed() {
-    return cryptogramsPlayed;
-}
+
+    public Player(String name, double acc, int numGuess,int numPlayed,int numCompleted){
+        username = name;
+        accuracy= acc;
+        totalGuesses = numGuess;
+        cryptogramsPlayed = numPlayed;
+        cryptogramsCompleted = numCompleted;
+    }    
+    public void updateAccuracy(double acc){
+        double combAcc = accuracy + acc;
+        accuracy = combAcc/2;
+    }
+    public void incrementCryptogramsCompleted(){
+        cryptogramsCompleted = cryptogramsCompleted + 1;
+    }
+    public void incrementCryptogramsPlayed(){
+        cryptogramsPlayed = cryptogramsPlayed + 1;
+    }
+    public Double getAccuracy() {
+        return accuracy;
+    }
+    public int getNumCryptogramsCompleted() {
+        return cryptogramsCompleted;
+    }
+    public int getNumCryptogramsPlayed() {
+        return cryptogramsPlayed;
+    }
 
 }
--- Players.java
+++ Players.java
@@ -1,1 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Players{
+    List<Player> allPlayers = new ArrayList<Player>();
+    public void LoadPlayers(String[] args){
+        
+        File myObj = new File("AllPlayers.txt");
+        try (Scanner in = new Scanner(myObj)) {
+            String[] data = null;
+            
+            while (in.hasNextLine()) {
+                
+                data = in.nextLine().split(" ");
+                addPlayer(data[0],Integer.valueOf(data[1]),Integer.valueOf(data[2]),Integer.valueOf(data[3]),Integer.valueOf(data[4]));
+                //allPlayers.add(new Player(data[0],Integer.valueOf(data[1]),Integer.valueOf(data[2]),Integer.valueOf(data[3]),Integer.valueOf(data[4])));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+    
+
+    }
+    public void addPlayer(String name, double acc, int numGuess, int numPlayed, int numCompleted){
+        allPlayers.add(new Player(name,acc,numGuess,numPlayed,numCompleted));
+    }
+    public void savePlayer(String name, double acc, int numGuess, int numPlayed, int numCompleted){
+        try{ 
+            FileWriter mywriter = new FileWriter("AllPlayer.txt");
+            mywriter.write(name +" "+ acc +" "+ numGuess +" "+ numPlayed +" "+ numCompleted);
+            mywriter.close();
+            System.out.println("player successfully added");
+        } catch (IOException e) {
+            System.out.println("An error occured");
+            e.printStackTrace();
+        }
+    }
+    public void findPlayer(String name){
+        File myObj = new File("AllPlayers.txt");
+        try (Scanner in = new Scanner(myObj)) {
+            String[] data = null;
+            while (in.hasNextLine()) {
+                
+                data = in.nextLine().split(" ");
+                if (data[0] == name){    
+                    System.out.println(data);
+                    break;
+                }
+             }
+        }catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+    }
+
+
+}
 
