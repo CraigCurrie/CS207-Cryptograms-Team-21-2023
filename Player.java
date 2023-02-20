ba7080c4f9b24f7818521e4b752674c1b05056f0
CraigCurrie <CraigCurrie118@gmail.com>
2023-02-20 12:03:36 +0000

    Changed NumberCryptogram and LetterCryptogram to java files and added variables and functions to the player class

--- Player.java
+++ Player.java
@@ -1,1 +1,28 @@
+public class Player{
+    String username;
+    Double accuracy;
+    int totalGuesses;
+    int cryptogramsPlayed;
+    int cryptogramsCompleted;
+    
+public void updateAccuracy(double acc){
+    double combAcc = accuracy + acc;
+    accuracy = combAcc/2;
+}
+public void incrementCryptogramsCompleted(){
+    cryptogramsCompleted = cryptogramsCompleted + 1;
+}
+public void incrementCryptogramsPlayed(){
+    cryptogramsPlayed = cryptogramsPlayed + 1;
+}
+public Double getAccuracy() {
+    return accuracy;
+}
+public int getNumCryptogramsCompleted() {
+    return cryptogramsCompleted;
+}
+public int getNumCryptogramsPlayed() {
+    return cryptogramsPlayed;
+}
 
+}
