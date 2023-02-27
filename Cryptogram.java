954dcd772c135da74111e2bffd8fe2e11fb90602
matteomcc <matteomccaughey@gmail.com>
2023-02-27 14:26:20 +0000

    getters and setters for cryptogram

--- Cryptogram.java
+++ Cryptogram.java
@@ -1,1 +1,33 @@
+public class Cryptogram{
 
+    String cryptogramPhrase;
+    String cypher;
+
+    public Cryptogram(String phrase, String cryptogramAlphabet){
+        cryptogramPhrase = phrase;
+        cypher = cryptogramAlphabet; 
+
+    }
+
+    public String getPhrase(){
+        return cryptogramPhrase;
+    }
+
+    public void setPhrase(String phrase){
+        cryptogramPhrase = phrase;
+    }
+
+    public String getCypher(){
+        return cypher;
+    } 
+
+    public void setCypher(String alphabet){
+        cypher = alphabet; 
+    }
+
+
+    public int getFrequencies(String alphabet, String phrase){
+ 
+        return 0;
+    }
+}
