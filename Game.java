3bc093594284c93db073f81d0a0d6aee2497c587
CraigCurrie <CraigCurrie118@gmail.com>
2023-03-02 11:09:00 +0000

    added rem function and fail check + message output

--- Game.java
+++ Game.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,11 +13,15 @@ public class Game{
     Players GamePlayers = new Players();
     private List<Cryptogram> allCryptograms = new ArrayList<Cryptogram>();
     private List<Character> currentGame = new ArrayList<Character>();
+    //frequency TO DO
     private List<Character> currentGameJumbled = new ArrayList<Character>();
     private List<Integer> currentGameMap = new ArrayList<Integer>();
     private List<String> guessPath = new ArrayList<String>();
+    private Integer numGuesses = 0;
+    private Integer numCorrectGuesses = 0;
+    private Cryptogram globalCurrCryptogram = new Cryptogram("", "");
 
-   public Game(){
+   public Game() throws IOException{
         currentPlayer = new Player(null, 0, 0, 0, 0);
         cryptoGame = new Cryptogram("", "");
 
@@ -32,6 +37,11 @@ public class Game{
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             e.printStackTrace();
+        }
+        //check for if there is cryptograms in file if not, throws error
+        if(allCryptograms.size() == 0){
+            throw new IOException("No cryptograms found");
+            
         }
     }
     
@@ -66,27 +76,40 @@ public class Game{
     }
     public void playGame(){
         Cryptogram currentCryptogram = generateCryptogram();
+        globalCurrCryptogram = currentCryptogram;
         Boolean running = true;
         Scanner in = new Scanner(System.in);
         for(int i = 0; i < currentGame.size(); i++){
             currentGameJumbled.add(currentCryptogram.cypher.charAt(i)); 
         }
+        //increases the current Players games played by 1
+        currentPlayer.incrementCryptogramsPlayed();
         while(running == true){
             String[] data = null;
             System.out.println(currentGame);
-            System.out.println("please enter your guess and its position (e.g c 1) enter undo to undo your previous guess");
+            System.out.println("please enter your guess and its position (e.g c 1) enter undo to undo your previous guess, to remove a guess type rem and the position you want to remove (e.g rem 1)");
             data = in.nextLine().split(" ");
             if (data[0].equals("undo")){
                 String[] guessPathData = null;
                 guessPathData = guessPath.get(guessPath.size()-1).split(" "); 
                 undoLetter(guessPathData[0].charAt(0), guessPathData[1].charAt(0), Integer.valueOf(guessPathData[2]));
 
-            }else if (Integer.valueOf(data[1]) < currentGameMap.size()){
+            }else if(data[0].equals("rem")){
+                remLetter(Integer.valueOf(data[1]));
+            }
+            else if (Integer.valueOf(data[1]) < currentGameMap.size()){
                 enterLetter(data[0].charAt(0),Integer.valueOf(data[1]));
                 if (currentGame.toString().substring(1, 3 * currentGame.size() - 1).replaceAll(", ", "").equals((currentCryptogram.cryptogramPhrase).replaceAll(" ",""))){
                     System.out.println(currentGame);
                     System.out.println("well done! The cryptogram has been solved!");
+                    //increases the current Players games completed by 1
+                    currentPlayer.incrementCryptogramsCompleted();;
+                    //updates the current Players Accuracy
+                    currentPlayer.updateAccuracy(numCorrectGuesses * 100/numGuesses);
                     running = false;
+                }else if ((currentGame.size() == currentGameMap.size()) && (currentGame.contains('_') == false)){
+                    System.out.println("Your current guess is not correct, try again");
+                    //update stats here if needed
                 }
             }
             else{
@@ -127,6 +150,10 @@ public class Game{
                 currentGame.set(i,c);
             }
         }
+        if(currentGame.get(pos) == globalCurrCryptogram.cryptogramPhrase.charAt(pos)){
+            numCorrectGuesses++;
+        }
+        numGuesses++;
     }
     public void undoLetter(char n, char o, int pos){
         for(int i = 0; i < currentGameMap.size(); i++){
@@ -135,6 +162,10 @@ public class Game{
                 currentGame.set(i, o);
             }
         }
+    }
+    public void remLetter(int pos){
+        guessPath.add(String.valueOf('_' +" "+ currentGame.get(pos) +" "+ pos));
+        currentGame.set(pos, '_');
     }
     public void viewFrequencies(){
         System.out.println(Arrays.toString(cryptoGame.getFrequencies()));
@@ -148,18 +179,18 @@ public class Game{
     public void showSolution(){
 
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         Game G = new Game();
         G.loadPlayer();
         //test to check saving
         //G.currPlayer.updateAccuracy(50); 
         //System.out.println(G.currPlayer.getAccuracy());
-        G.savePlayer();
         //G.viewFrequencies();
         //test to check genCrypto
         //Cryptogram C = G.generateCryptogram();
         //System.out.printf(C.cryptogramPhrase +" "+ C.cypher);
         G.playGame();
+        G.savePlayer();
     }
 }
     
